d2b80c30ddcfca24868f8b7b5ea1c09c31d403db
devin-garg <devin.garg@s-sm.org>
2018-12-05 15:26:24 -0800

    DataSet loader

---
+++ DataSet.py
@@ -0,0 +1,19 @@
+from torch.utils import data
+class Dataset(data.Dataset):
+    def __init__(self, path, transform):
+        self.ids = []
+        self.label = path
+        self.transform = transform
+        for f in os.listdir(path):            
+            if not f.startswith('.'):
+                self.ids.append(f)
+    def __len__(self):
+        return len(self.ids)
+    def __getitem__(self, index):
+        y, sr = librosa.load(label + "/" + self.ids[index])
+        X = librosa.feature.melspectrogram(y=y, sr=sr)
+        if self.transform is not None:
+            X = self.transform(X)
+        label = self.label
+        return X, label
+        
