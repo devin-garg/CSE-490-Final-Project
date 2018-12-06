e3c0df4472aa6eb189ecfa987a28daedb66daf13
dd292 <dd292@uw.edu>
2018-12-05 18:28:47 -0800

    changed a few things

--- train.py
+++ train.py
@@ -1,4 +1,11 @@
 import matplotlib.pyplot as plt
+from DataSet import Dataset
+import Test_train
+from InstrumentNet import InstrumentNet
+import torch
+import torch.optim as optim
+import pt_util
+from torchvision import transforms
 # Play around with these constants, you may find a better setting.
 BATCH_SIZE = 256
 TEST_BATCH_SIZE = 10
@@ -8,7 +15,29 @@ MOMENTUM = 0.9
 USE_CUDA = True
 PRINT_INTERVAL = 100
 WEIGHT_DECAY = 0.0005
-LOG_PATH = DATA_PATH + 'log.pkl'
+
+####INPUTS To Change####
+trainPath = 'nsynth-valid/audio/'
+testPath = 'nsynth-test/audio/'
+LOG_PATH = 'logs/log.pkl'
+
+
+train_transforms = transforms.Compose([
+    transforms.ToPILImage(),
+    transforms.Resize([128,128]),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.5,0.5,0.5],std=[0.5,0.5,0.5]),
+])
+test_transforms = transforms.Compose([
+    transforms.ToPILImage(),
+    transforms.Resize([128,128]),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.5,0.5,0.5],std=[0.5,0.5,0.5]),
+])
+data_train = Dataset(trainPath, transform=train_transforms)
+data_test = Dataset(testPath, transform=test_transforms)
+
+###################################################
 
 loss=[]
 Acc=[]
@@ -24,9 +53,7 @@ print('num cpus:', multiprocessing.cpu_count())
 kwargs = {'num_workers': multiprocessing.cpu_count(),
           'pin_memory': True} if use_cuda else {}
 
-class_names = [line.strip().split(', ') for line in open(DATA_PATH + 'class_names.txt')]
-name_to_class = {line[1]: line[0] for line in class_names}
-class_names = [line[1] for line in class_names]
+
 
 train_loader = torch.utils.data.DataLoader(data_train, batch_size=BATCH_SIZE,
                                            shuffle=True, **kwargs)
@@ -35,24 +62,24 @@ test_loader = torch.utils.data.DataLoader(data_test, batch_size=TEST_BATCH_SIZE,
 
 model = InstrumentNet().to(device)
 optimizer = optim.SGD(model.parameters(), lr=LEARNING_RATE, momentum=MOMENTUM, weight_decay=WEIGHT_DECAY)
-start_epoch = model.load_last_model(DATA_PATH + 'checkpoints')
+start_epoch = model.load_last_model('checkpoints/')
 
 # You may want to define another default for your log data depending on how you save it.
 log_data = pt_util.read_log(LOG_PATH, [])
 
-correct_images, correct_val, error_images, predicted_val, gt_val,accuracy,Testloss = test(model, device, test_loader, True)
+correct_images, correct_val, error_images, predicted_val, gt_val,accuracy,Testloss = Test_train.test(model, device, test_loader, True)
 correct_images = pt_util.to_scaled_uint8(correct_images.transpose(0, 2, 3, 1))
 error_images = pt_util.to_scaled_uint8(error_images.transpose(0, 2, 3, 1))
-pt_util.show_images(correct_images, ['correct: %s' % class_names[aa] for aa in correct_val])
-pt_util.show_images(error_images, ['pred: %s, actual: %s' % (class_names[aa], class_names[bb]) for aa, bb in zip(predicted_val, gt_val)])
+#pt_util.show_images(correct_images, ['correct: %s' % class_names[aa] for aa in correct_val])
+#pt_util.show_images(error_images, ['pred: %s, actual: %s' % (class_names[aa], class_names[bb]) for aa, bb in zip(predicted_val, gt_val)])
 
 try:
     for epoch in range(start_epoch, EPOCHS + 1):
         loss.append(Testloss)
         Acc.append(accuracy)
-        trainloss=train(model, device, train_loader, optimizer, epoch, PRINT_INTERVAL)
+        trainloss=Test_train.train(model, device, train_loader, optimizer, epoch, PRINT_INTERVAL)
         Train_loss.append(trainloss)
-        correct_images, correct_val, error_images, predicted_val, gt_val,accuracy,Testloss = test(model, device, test_loader, True)
+        correct_images, correct_val, error_images, predicted_val, gt_val,accuracy,Testloss = Test_train.test(model, device, test_loader, True)
         model.save_best_model(accuracy,DATA_PATH + 'checkpoints/%03d.pt' % epoch, 0)
         # TODO define other things to do at the end of each loop like logging and saving the best model.
 
